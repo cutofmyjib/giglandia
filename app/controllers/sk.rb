ff0a7829e0702d25773e22f74a09f5ce768ca5d8
cutofmyjib <thedianalozano@gmail.com>
2015-09-07 14:23:19 -0700

    refactor find_artist method

--- app/controllers/sk.rb
+++ app/controllers/sk.rb
@@ -1,19 +1,21 @@
-require 'dotenv'
-Dotenv.load
-
 module Songkick
   class Client
     include HTTParty
 
-    base_uri "http://api.songkick.com/api/3.0/"
+    base_uri "http://api.songkick.com/api/3.0"
+
+    def initialize
+      @apikey = ENV['SONGKICK_KEY']
+    end
 
     def find_artist(band_name)
-      response = self.class.get("/search/artists.json?",
-        :headers => { :apikey => ENV['SONGKICK_KEY']},
-        :query => { :query => band_name }
-        )
+      response =  self.class.get("/search/artists.json",
+                  query: { query: band_name,
+                           apikey: ENV['SONGKICK_KEY'] }
+      )
 
       return response.to_hash
     end
+
   end
 end
